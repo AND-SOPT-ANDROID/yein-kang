9f08f268c156ab12d66b40d8a6ce86ca6649e97b
kangyein9892 <inwon9892@naver.com>
2024-10-10 14:54:42 +0900

    ui/#2: action 버튼 있는 textfield 컴포넌트 구현

---
+++ app/src/main/java/org/sopt/and/component/WavveActionTextField.kt
@@ -0,0 +1,49 @@
+package org.sopt.and.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import org.sopt.and.R
+
+@Composable
+fun WavveActionTextField(
+    value: String,
+    hint: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier,
+){
+    var showPassword by remember { mutableStateOf(false) }
+
+    WavveTextField(
+        value = value,
+        hint = hint,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done
+        ),
+        visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+        actionButton = {
+            Text(
+                text = if (showPassword) stringResource(R.string.text_field_hide)
+                else stringResource(R.string.text_field_show),
+                color = Color.White,
+                modifier = Modifier
+                    .clickable {
+                        showPassword = !showPassword
+                    }
+            )
+        },
+        modifier = modifier
+    )
+}
