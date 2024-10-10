21bcd6fac79569cd2cc4e7397a13fb60684441f9
kangyein9892 <inwon9892@naver.com>
2024-10-10 15:13:54 +0900

    feat/#2: 로그인 intent handled 함수 구현

--- app/src/main/java/org/sopt/and/signin/SignInActivity.kt
+++ app/src/main/java/org/sopt/and/signin/SignInActivity.kt
@@ -1,4 +1,30 @@
 
+    private fun handledIntent(state: MutableState<SignInState>, intent: SignInIntent) {
+        when (intent) {
+            is SignInIntent.EnterId -> {
+                state.value = state.value.copy(id = intent.id)
+            }
+            is SignInIntent.EnterPassword -> {
+                state.value = state.value.copy(password = intent.password)
+            }
+            is SignInIntent.SignIn -> {
+                if (isValidatedSignIn(state.value.id, state.value.password)) {
+                    state.value = state.value.copy(
+                        signInSuccessful = true,
+                        snackBarMessage = getString(R.string.signin_success_text)
+                    )
+
+
+                } else {
+                    state.value = state.value.copy(
+                        snackBarMessage = getString(R.string.signin_failure_text)
+                    )
+                }
+
+            }
+        }
+    }
+
     private fun isValidatedSignIn(id: String, password: String): Boolean{
         return signUpState.let {
             it.value.id == id && it.value.password == password
