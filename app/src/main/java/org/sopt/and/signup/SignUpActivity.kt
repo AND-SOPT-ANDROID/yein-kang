b6d38d6dd44b8cb58a00bce0caae17a021388788
kangyein9892 <inwon9892@naver.com>
2024-10-10 15:07:17 +0900

    feat/#2: 회원가입 activity, 기능 구현

--- app/src/main/java/org/sopt/and/signup/SignUpActivity.kt
+++ app/src/main/java/org/sopt/and/signup/SignUpActivity.kt
@@ -1,3 +1,80 @@
+package org.sopt.and.signup
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import org.sopt.and.R
+import org.sopt.and.component.CloseTopBar
+import org.sopt.and.signup.Intent.SignUpIntent
+import org.sopt.and.signup.model.SignUpState
+import org.sopt.and.ui.theme.ANDANDROIDTheme
+import org.sopt.and.ui.theme.FirstGrey
+
+class SignUpActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            ANDANDROIDTheme {
+                val snackbarHostState = remember { SnackbarHostState() }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = { SnackbarHost(snackbarHostState) },
+                    topBar = {
+                        CloseTopBar(
+                            title = stringResource(R.string.signup_top_bar_title),
+                            onBtnClick = {
+                                finish()
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+
+                    val signUpState = remember { mutableStateOf(SignUpState()) }
+
+                    SignUpScreen(
+                        state = signUpState.value,
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .background(color = FirstGrey),
+                        onIdChange = {
+                            handledIntent(signUpState, SignUpIntent.EnterId(it))
+                        },
+                        onPasswordChange = {
+                            handledIntent(signUpState, SignUpIntent.EnterPassword(it))
+                        },
+                        onSignUpButtonClick = {
+                            handledIntent(signUpState, SignUpIntent.SignUp)
+                        }
+                    )
+
+                    signUpState.value.snackBarMessage?.let { message ->
+                        LaunchedEffect(snackbarHostState) {
+                            snackbarHostState.showSnackbar(message)
+                            signUpState.value = signUpState.value.copy(snackBarMessage = null)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private fun handledIntent(state: MutableState<SignUpState>, signUpIntent: SignUpIntent) {
         when (signUpIntent) {
             is SignUpIntent.EnterId -> {
