07ca6c08eb7011aef80e559230fc4484b6e19291
kangyein9892 <inwon9892@naver.com>
2024-10-10 15:15:16 +0900

    ui/#2: 마이페이지에 필요한 컴포넌트 구현

---
+++ app/src/main/java/org/sopt/and/component/ContentsView.kt
@@ -0,0 +1,64 @@
+package org.sopt.and.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.R
+import org.sopt.and.ui.theme.ThirdGrey
+
+@Composable
+fun ContentsView(
+    title: String,
+    contentText: String,
+    titleColor: Color = Color.White,
+    contentTextColor: Color = ThirdGrey,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            color = titleColor,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Info,
+                contentDescription = stringResource(R.string.icon_info),
+                tint = contentTextColor,
+                modifier = Modifier.size(56.dp)
+            )
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+
+            Text(
+                text = contentText,
+                color = contentTextColor,
+                fontSize = 12.sp
+            )
+        }
+    }
+}
---
+++ app/src/main/java/org/sopt/and/component/PairTextView.kt
@@ -0,0 +1,49 @@
+package org.sopt.and.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.and.R
+import org.sopt.and.ui.theme.ThirdGrey
+
+@Composable
+fun PairTextView(
+    title: String,
+    subTitle: String,
+    titleColor: Color = ThirdGrey,
+    subTitleColor: Color = Color.White,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+    ){
+        Text(
+            text = title,
+            color = titleColor,
+        )
+
+        Row{
+            Text(
+                text = subTitle,
+                color = subTitleColor,
+                modifier = Modifier.padding(top = 1.dp)
+            )
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowRight,
+                contentDescription = stringResource(R.string.icon_arrow_right),
+                tint = titleColor,
+            )
+        }
+    }
+}
