0f70c46dc219591037166838f338b421a305607a
kangyein9892 <inwon9892@naver.com>
2024-10-10 15:06:51 +0900

    feat/#2: 회원가입 intent handled 함수 구현

--- app/src/main/java/org/sopt/and/signup/SignUpActivity.kt
+++ app/src/main/java/org/sopt/and/signup/SignUpActivity.kt
@@ -1,3 +1,41 @@
+    private fun handledIntent(state: MutableState<SignUpState>, signUpIntent: SignUpIntent) {
+        when (signUpIntent) {
+            is SignUpIntent.EnterId -> {
+                state.value = state.value.copy(id = signUpIntent.id)
+            }
+            is SignUpIntent.EnterPassword -> {
+                state.value = state.value.copy(password = signUpIntent.password)
+            }
+            is SignUpIntent.SignUp -> {
+                val currentState = state.value
+                val isIdValid = isValidatedId(currentState.id)
+                val isPasswordValid = isValidatedPassword(currentState.password)
+
+                when {
+                    !isIdValid -> {
+                        state.value = currentState.copy(
+                            snackBarMessage = getString(R.string.signup_failure_email_text)
+                        )
+                    }
+                    !isPasswordValid -> {
+                        state.value = currentState.copy(
+                            snackBarMessage = getString(R.string.signup_failure_password_text)
+                        )
+                    }
+                    else -> {
+                        state.value = currentState.copy(
+                            signUpSuccessful = true,
+                        )
+                        intent.putExtra("id", currentState.id)
+                        intent.putExtra("password", currentState.password)
+                        intent.putExtra("snackBarMessage", getString(R.string.signup_success_text))
+                        setResult(Activity.RESULT_OK, intent)
+                        finish()
+                    }
+                }
+            }
+        }
+    }
     private fun isValidatedId(id: String): Boolean{
         val emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
         return id.matches(emailPattern.toRegex())
