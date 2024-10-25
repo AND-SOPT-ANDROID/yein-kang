d8b56125f587f4b30048b4a4fcd04f1df747b961
kangyein9892 <inwon9892@naver.com>
2024-10-26 02:27:56 +0900

    feat/#3: SignUpViewModel 구현

---
+++ app/src/main/java/org/sopt/and/sign/signup/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,56 @@
+package org.sopt.and.sign.signup.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.sopt.and.R
+import org.sopt.and.sign.signup.intent.SignUpIntent
+import org.sopt.and.sign.signup.model.SignUpState
+
+class SignUpViewModel: ViewModel() {
+
+    private var _state = MutableStateFlow(SignUpState())
+    val state = _state.asStateFlow()
+
+    private var _intent = MutableSharedFlow<SignUpIntent>()
+    val intent = _intent.asSharedFlow()
+
+    fun updateId(id: String) = _state.update {
+        it.copy(id = id)
+    }
+
+    fun updatePassword(password: String) = _state.update {
+        it.copy(password = password)
+    }
+
+    fun onSignUpButtonClick() = viewModelScope.launch {
+        val isValidateId = state.value.id.matches(emailPattern)
+        val isValidatePassword = state.value.password.matches(passwordPattern)
+        when{
+            isValidateId && isValidatePassword -> {
+                _intent.emit(SignUpIntent.SnackBar(R.string.signup_success_text))
+                _intent.emit(SignUpIntent.SignUp)
+            }
+            !isValidateId -> {
+                _intent.emit(SignUpIntent.SnackBar(R.string.signup_failure_email_text))
+            }
+            !isValidatePassword -> {
+                _intent.emit(SignUpIntent.SnackBar(R.string.signup_failure_password_text))
+            }
+            else -> {
+                _intent.emit(SignUpIntent.SnackBar(R.string.signup_failure_text))
+            }
+        }
+    }
+
+    companion object {
+        val emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
+        val passwordPattern = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,20}$".toRegex()
+    }
+}
+
