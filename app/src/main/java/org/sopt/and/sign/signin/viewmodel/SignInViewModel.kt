5dd25782f7c1adfcb00cc03d001b2df500a179b2
kangyein9892 <inwon9892@naver.com>
2024-10-26 02:23:47 +0900

    feat/#3: SignInViewModel 구현

---
+++ app/src/main/java/org/sopt/and/sign/signin/viewmodel/SignInViewModel.kt
@@ -0,0 +1,49 @@
+package org.sopt.and.sign.signin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.sopt.and.R
+import org.sopt.and.sign.signin.intent.SignInIntent
+import org.sopt.and.sign.signin.model.SignInState
+
+class SignInViewModel: ViewModel() {
+
+    private var _state = MutableStateFlow(SignInState())
+    val state = _state.asStateFlow()
+
+    private var _intent = MutableSharedFlow<SignInIntent>()
+    val intent = _intent.asSharedFlow()
+
+    fun updateId(id: String) = _state.update {
+        it.copy(id = id)
+    }
+
+    fun updatePassword(password: String) = _state.update {
+        it.copy(password = password)
+    }
+
+    fun onSignUpButtonClick() = viewModelScope.launch {
+        _intent.emit(SignInIntent.SignUp)
+    }
+
+    fun onSignInButtonClick(id: String, password: String) = viewModelScope.launch {
+        if(isValidateSignIn(id, password)) {
+            _intent.emit(SignInIntent.SnackBar(R.string.signin_success_text))
+            _intent.emit(SignInIntent.SignIn)
+        } else {
+            _intent.emit(SignInIntent.SnackBar(R.string.signin_failure_text))
+        }
+    }
+    private fun isValidateSignIn(id: String, password: String): Boolean {
+        return state.value.let {
+            it.id == id && it.password == password
+        }
+    }
+
+}
