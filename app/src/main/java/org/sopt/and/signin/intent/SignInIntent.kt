8ce2da021653d315a64e6c949bfe2c91ee26c1b3
kangyein9892 <inwon9892@naver.com>
2024-10-26 02:23:06 +0900

    mod/#3: signin intent 및 model(state) 수정

--- app/src/main/java/org/sopt/and/signin/intent/SignInIntent.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.and.signin.intent
-
-sealed class SignInIntent {
-    data class EnterId(val id: String) : SignInIntent()
-    data class EnterPassword(val password: String) : SignInIntent()
-    data object SignIn : SignInIntent()
-
-}
