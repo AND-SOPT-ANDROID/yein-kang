bcf6248d00068c111d082ef43c7b0c011345a178
kangyein9892 <inwon9892@naver.com>
2024-10-26 02:55:09 +0900

    del/#3: 필요없는 keyUtil 삭제

--- app/src/main/java/org/sopt/and/util/KeyUtil.kt
+++ app/src/main/java/org/sopt/and/util/KeyUtil.kt
@@ -3,7 +3,6 @@ package org.sopt.and.util
 object KeyUtil {
     const val ID = "id"
     const val PASSWORD = "password"
-    const val SNACK_BAR_MESSAGE = "snackBarMessage"
     const val DEFAULT_STRING = ""
     const val PREF_NAME = "wavve_prefs"
 }
