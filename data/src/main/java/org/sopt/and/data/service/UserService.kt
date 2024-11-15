ad8364e7c39a6cbe9b1181c0bd6dc723604b5f77
kangyein9892 <inwon9892@naver.com>
2024-11-15 16:25:12 +0900

    chore/#8: signUp dto 이름 오류 생긴 부분 수정

--- data/src/main/java/org/sopt/and/data/service/UserService.kt
+++ data/src/main/java/org/sopt/and/data/service/UserService.kt
@@ -1,7 +1,7 @@
 package org.sopt.and.data.service
 
-import org.sopt.and.data.dto.request.SignUpRequest
 import org.sopt.and.data.dto.BaseResponse
+import org.sopt.and.data.dto.request.SignUpRequestDto
 import org.sopt.and.data.dto.response.SignUpResponseDto
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 internal interface UserService {
     @POST("user")
     suspend fun signUp(
-        @Body request: SignUpRequest
+        @Body request: SignUpRequestDto
     ): BaseResponse<SignUpResponseDto>
 
 }
