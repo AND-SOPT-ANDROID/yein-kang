f6d33686e8154ad2db14f4e6da22843fcf065abd
kangyein9892 <inwon9892@naver.com>
2024-11-15 13:19:52 +0900

    mod/#8: SignUpSideEffect 및 SignUpState 수정
    hobby와 SnackBarText 추가

--- presentation/src/main/java/org/sopt/and/presentation/sign/signup/intent/SignUpSideEffect.kt
+++ presentation/src/main/java/org/sopt/and/presentation/sign/signup/intent/SignUpSideEffect.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 
 sealed class SignUpSideEffect {
     data class SnackBar(@StringRes val message: Int) : SignUpSideEffect()
+    data class SnackBarText(val message: String): SignUpSideEffect()
     data object SignUp: SignUpSideEffect()
 }
 
--- presentation/src/main/java/org/sopt/and/presentation/sign/signup/model/SignUpState.kt
+++ presentation/src/main/java/org/sopt/and/presentation/sign/signup/model/SignUpState.kt
@@ -4,5 +4,6 @@ import org.sopt.and.presentation.util.KeyUtil.DEFAULT_STRING
 
 data class SignUpState(
     val id: String = DEFAULT_STRING,
-    val password: String = DEFAULT_STRING
+    val password: String = DEFAULT_STRING,
+    val hobby: String = DEFAULT_STRING
 )
