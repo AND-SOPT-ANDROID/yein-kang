b5682fbd85ff70bf8373d92d0acf371bc3f98fd1
kangyein9892 <inwon9892@naver.com>
2024-11-15 13:21:40 +0900

    feat/#8: SignUpViewModel 취미 update, 회원가입, 네트워크 처리, intent 처리 구현

--- presentation/src/main/java/org/sopt/and/presentation/sign/signup/viewmodel/SignUpViewModel.kt
+++ presentation/src/main/java/org/sopt/and/presentation/sign/signup/viewmodel/SignUpViewModel.kt
@@ -9,21 +9,32 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.sopt.and.domain.exception.NetworkError
+import org.sopt.and.domain.exception.onError
+import org.sopt.and.domain.exception.onSuccess
+import org.sopt.and.domain.model.User
+import org.sopt.and.domain.repository.UserRepository
 import org.sopt.and.presentation.R
+import org.sopt.and.presentation.delegate.NetworkDelegate
 import org.sopt.and.presentation.sign.signup.intent.SignUpSideEffect
 import org.sopt.and.presentation.sign.signup.model.SignUpState
 import javax.inject.Inject
 
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
-
+    private val userRepository: UserRepository
 ): ViewModel() {
+
+    @Inject
+    lateinit var networkDelegate: NetworkDelegate
 
     private var _state = MutableStateFlow(SignUpState())
     val state = _state.asStateFlow()
 
     private var _intent = MutableSharedFlow<SignUpSideEffect>()
     val intent = _intent.asSharedFlow()
+
+    val networkState get() = networkDelegate.networkState
 
     fun updateId(id: String) = _state.update {
         it.copy(id = id)
@@ -33,29 +44,32 @@ class SignUpViewModel @Inject constructor(
         it.copy(password = password)
     }
 
-    fun onSignUpButtonClick() = viewModelScope.launch {
-        val isValidateId = state.value.id.matches(emailPattern)
-        val isValidatePassword = state.value.password.matches(passwordPattern)
-        when{
-            isValidateId && isValidatePassword -> {
-                _intent.emit(SignUpSideEffect.SnackBar(R.string.signup_success_text))
-                _intent.emit(SignUpSideEffect.SignUp)
-            }
-            !isValidateId -> {
-                _intent.emit(SignUpSideEffect.SnackBar(R.string.signup_failure_email_text))
-            }
-            !isValidatePassword -> {
-                _intent.emit(SignUpSideEffect.SnackBar(R.string.signup_failure_password_text))
-            }
-            else -> {
-                _intent.emit(SignUpSideEffect.SnackBar(R.string.signup_failure_text))
-            }
+    fun updateHobby(hobby: String) = _state.update {
+        it.copy(hobby = hobby)
+    }
+
+    fun signUp() = viewModelScope.launch {
+        val currentState = _state.value
+        val user = User(
+            username = currentState.id,
+            password = currentState.password,
+            hobby = currentState.hobby
+        )
+        userRepository.signUp(user).onSuccess {
+            networkDelegate.handleNetworkSuccess()
+        }.onError {
+            networkDelegate.handleSignUpError(it)
         }
     }
 
-    companion object {
-        val emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
-        val passwordPattern = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,20}$".toRegex()
+    suspend fun handleSignUpIntentError(message: String) {
+        _intent.emit(SignUpSideEffect.SnackBarText(message))
     }
+
+    suspend fun handleSignUpIntentSuccess() {
+        _intent.emit(SignUpSideEffect.SnackBar(R.string.signup_success_text))
+        _intent.emit(SignUpSideEffect.SignUp)
+    }
+
 }
 
