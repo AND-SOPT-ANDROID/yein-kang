92cbc24f14df9432afd9c94c3abf18ca84dc4a22
kangyein9892 <inwon9892@naver.com>
2024-11-15 21:41:46 +0900

    feat/#8: getMyHobby 에러 위임하는 기능 추가

--- presentation/src/main/java/org/sopt/and/presentation/delegate/NetworkDelegate.kt
+++ presentation/src/main/java/org/sopt/and/presentation/delegate/NetworkDelegate.kt
@@ -60,6 +60,30 @@ class NetworkDelegate @Inject constructor() {
         _networkState.value = NetworkState.Error(updatedException.title, updatedException.message)
     }
 
+    fun handleGetMyHobbyError(exception: NetworkError) {
+        val updatedException = when(exception){
+            is HttpCodeError -> {
+                when(exception.title) {
+                    "401" -> HttpCodeError(
+                        "요청에 문제가 있습니다. ",
+                        "토큰이 존재하지 않습니다."
+                    )
+                    "403" -> HttpCodeError(
+                        "요청에 문제가 있습니다. ",
+                        "토큰이 유효하지 않습니다."
+                    )
+                    "404" -> HttpCodeError(
+                        "요청에 문제가 있습니다. ",
+                        "유효하지 못한 요청입니다."
+                    )
+                    else -> UnknownError
+                }
+            }
+            else -> exception
+        }
+        _networkState.value = NetworkState.Error(updatedException.title, updatedException.message)
+    }
+
 
     fun handleNetworkSuccess(){
         _networkState.value = NetworkState.Success
